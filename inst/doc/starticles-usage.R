58938d1cf60f4f77daf0366a012f0c939fbc540a
David M. Kaplan <dmkaplan2000@gmail.com>
2022-06-09 06:50:02 +0000

    version 0.1.0

---
+++ inst/doc/starticles-usage.R
@@ -0,0 +1,15 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  remotes::install_github("dmkaplan2000/starticles",build_vignettes=TRUE)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  rmarkdown::draft("MYARTICLENAME.Rmd","generic_article","starticles",edit=FALSE)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  rmarkdown::render("MYARTICLENAME/MYARTICLENAME.Rmd")
+
